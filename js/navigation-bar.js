9c47592a19008c45a53b4f60f9e6b0944ed6eba5
xRamyun <xramyuntft@yahoo.com>
2024-03-15 19:14:36 -0400

    make nav bigger and remove form border when search button clicked

--- js/navigation-bar.js
+++ js/navigation-bar.js
@@ -1,3 +1,10 @@
 function navSearchClicked() {
-  prompt('test');
+  extendSearch();
+}
+
+function extendSearch () {
+  let navBar = document.querySelector('.nav-bar');
+  let form = document.querySelector('#nav-form');
+  navBar.style.height = "65vh";
+  form.style.border = "none";
 }
