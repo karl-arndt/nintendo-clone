9617aa689d98582a0f4acba3b6ac2816ae31df36
xRamyun <xramyuntft@yahoo.com>
2024-03-15 19:55:14 -0400

    move onclicked nav searchbar before categories select

--- js/navigation-bar.js
+++ js/navigation-bar.js
@@ -1,13 +1,17 @@
 function navSearchClicked() {
-  extendSearch();
   removeElements();
+  extendSearch();
+  
 }
 
 function extendSearch () {
   document.querySelector('.nav-bar').style.height = "65vh";
+  document.querySelector('#nav-form').insertBefore(
+    document.createElement('input'), document.querySelector('#categories'));
 }
 
 function removeElements() {
   document.querySelector('#nav-form').style.border = "none";
   document.querySelector('#right-nav').remove();
+  document.querySelector('#search-button').remove();
 }
